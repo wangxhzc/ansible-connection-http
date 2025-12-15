801fb89982fb8e45683d21df9e2ac1d6ad91806c
wangxh <wangxh@koal.com>
2025-12-15 15:24:06 +0800

    feat: 更新License

--- connection_agent/start.sh
+++ connection_agent/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# Copyright 2025 wangxhzc
+# SPDX-License-Identifier: Apache-2.0
 
 if [ ! -f server.key ]; then
   openssl genrsa -out server.key 2048
 fi
 
 if [ ! -f server.pem ]; then
-  openssl req -new -x509 -key server.key -out server.pem -days 365 -subj "/C=CN/ST=SHANGHAI/L=SHANGHAI/O=muc/OU=muc/CN=server.muc"
+  openssl req -new -x509 -key server.key -out server.pem -days 365 -subj "/C=CN/ST=SHANGHAI/L=SHANGHAI/O=wangxhzc/OU=wangxhzc/CN=server.wangxhzc"
 fi
 
 export AGENT_LOG_LEVEL=ERROR
